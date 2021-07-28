105c5768b7baa4c957d4b03b53bac38542d51f2c
Ray C Horn <raychorn@gmail.com>
2021-07-28 22:20:58 +0000

    updates.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
+docker node update --label-add mongo.role=data1 server1
+docker node update --label-add mongo.role=cfg1 server1
+
+docker node update --label-add mongo.role=cfg2 server-jj95enl
+docker node update --label-add mongo.role=data2 server-jj95enl
+docker node update --label-add mongo.role=mongos1 server-jj95enl
+
+docker node update --label-add mongo.role=cfg3 tp01-2066
+docker node update --label-add mongo.role=data3 tp01-2066
+docker node update --label-add mongo.role=mongos2 tp01-2066
+
 docker network create --attachable -d overlay mongo
 docker network create --attachable -d overlay mongos
 
-docker stack deploy -c swarm-compose.yml mongo
+#docker stack deploy -c swarm-compose.yml mongo
